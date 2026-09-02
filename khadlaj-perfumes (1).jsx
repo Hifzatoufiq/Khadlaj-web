d21f60a05a2006026e11bebd6611c2b740a9ee68
hifza <hifzatoufiq@gmail.com>
2026-09-02 10:27:30 +0500

    Replace dull/blurry banners with vibrant ultra-HD 1920x880 commercial quality banners and match slider aspect ratio

--- khadlaj-perfumes (1).jsx
+++ khadlaj-perfumes (1).jsx
@@ -5398,7 +5398,7 @@ function NewLaunchesBannerSlider({ setPage, setViewProduct }) {
         <div style={{
           position: "relative",
           width: "100%",
-          aspectRatio: "3/1",
+          aspectRatio: "1920/880",
           borderRadius: "14px",
           overflow: "hidden",
           boxShadow: "0 14px 38px rgba(37,23,55,0.09)",
